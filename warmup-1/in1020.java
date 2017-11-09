65351b8fa5810c0b5144184b5fd248a0db4650ed
Neota Moe <neota.moe@gmail.com>
2017-11-08 20:42:59 -0600

    1020 done

---
+++ warmup-1/in1020.java
@@ -0,0 +1,6 @@
+// Given 2 int values, return true if either of them is in the range 10..20 inclusive.
+
+// My Solution:
+public boolean in1020(int a, int b) {
+  return ((a >=10 && a<=20) || (b>=10 && b<=20));
+}

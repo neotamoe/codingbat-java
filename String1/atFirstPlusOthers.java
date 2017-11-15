9b8037f9a6074a97ba44674df0d7ad48aadbbecc
Neota Moe <neota.moe@gmail.com>
2017-11-14 20:28:28 -0600

    one more done

--- String1/atFirstPlusOthers.java
+++ String1/atFirstPlusOthers.java
@@ -147,3 +147,38 @@ public String withoutX(String str) {
   }
   return result;
 }
+
+
+// Given a string, if one or both of the first 2 chars is 'x', return the string
+// without those 'x' chars, and otherwise return the string unchanged. This is a
+// little harder than it looks.
+// My Solution & Comments: Not my most efficient coding; not refactoring at this time.
+public String withoutX2(String str) {
+  String result = "";
+  if(str.length()==2 && str.substring(0).equals("xx")){
+    return "";
+  }
+  if(str.length()==2 && str.substring(0,1).equals("x")){
+    return str.substring(1);
+  }
+  if(str.length()==2 && str.substring(1).equals("x")){
+    return str.substring(0,1);
+  }
+
+  if(str.length()==1 && str.substring(0,1).equals("x")){
+    return "";
+  }
+  if(str.length()==0){
+    return "";
+  }
+  if(str.substring(0,1).equals("x") && str.substring(1,2).equals("x") ){
+    return str.substring(2);
+  }
+  if(str.substring(0,1).equals("x")){
+    return str.substring(1);
+  }
+  if(str.substring(1,2).equals("x")){
+    return str.substring(0,1) + str.substring(2);
+  }
+  return str;
+}

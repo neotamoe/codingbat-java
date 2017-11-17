83e825de87dc9dadc60088121ab2cdf0f5720688
Neota Moe <neota.moe@gmail.com>
2017-11-16 23:29:34 -0600

    one more

--- Array-1/firstLast6AndOthers.java
+++ Array-1/firstLast6AndOthers.java
@@ -105,3 +105,8 @@ public int[] makeEnds(int[] nums) {
 public boolean has23(int[] nums) {
   return (nums[0]==2 || nums[0]==3 || nums[1]==2 || nums[1]==3);
 }
+
+// Given an int array length 2, return true if it does not contain a 2 or 3.
+public boolean no23(int[] nums) {
+  return (nums[0]!=2 && nums[0]!=3 && nums[1]!=2 && nums[1]!=3);
+}

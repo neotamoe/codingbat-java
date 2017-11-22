0412a5efda3e3f18b933032671a49b9b17a27f34
Neota Moe <neota.moe@gmail.com>
2017-11-21 19:59:12 -0600

    refactored makeBricks

--- logic-2/makeBricks.java
+++ logic-2/makeBricks.java
@@ -3,6 +3,8 @@
 // possible to make the goal by choosing from the given bricks. This is a little
 // harder than it looks and can be done without any loops.
 
+// My Solution passes all of the listed tests, but does not pass for other tests (tests that
+// aren't shown).
 public boolean makeBricks(int small, int big, int goal) {
   Boolean canDo = false;
   // if goal is a multiple of 5, check if there are enough 5 length bricks
@@ -19,5 +21,19 @@ public boolean makeBricks(int small, int big, int goal) {
   return canDo;
 }
 
-// My Solution passes all of the listed tests, but does not pass for other tests (tests that
-// aren't shown).
+// My second solution passes all tests.  I did not watch the solution video, but
+// did look at the hints provided (TIPS: Fail 1: not enough bricks; Fail 2: not enough
+// small bricks).  Based on that, I re-coded the solution.  
+public boolean makeBricks(int small, int big, int goal) {
+  Boolean canDo = false;
+  if(goal<=((big*5)+small)){
+    if(goal-(big*5)<=small && goal-(big*5)>=0){
+      canDo = true;
+    } else if (((goal-small)%5)==0 && ((goal-small)/5)<=big){
+      canDo = true;
+    } else if (goal%5<=small && goal/5<=big){
+      canDo = true;
+    }
+  }
+  return canDo;
+}

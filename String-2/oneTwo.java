c1a8a58153ff9209150483b9fa8863cd4e472871
Neota Moe <neota.moe@gmail.com>
2017-11-23 07:07:16 -0600

    one more, happy thanksgiving

---
+++ String-2/oneTwo.java
@@ -0,0 +1,13 @@
+// Given a string, compute a new string by moving the first char to come after the
+// next two chars, so "abc" yields "bca". Repeat this process for each subsequent
+// group of 3 chars, so "abcdef" yields "bcaefd". Ignore any group of fewer than 3
+// chars at the end.
+
+
+public String oneTwo(String str) {
+  String result = "";
+  for (int i=0; i<str.length()-2; i+=3){
+    result += str.substring(i+1,i+3) + str.substring(i,i+1);
+  }
+  return result;
+}

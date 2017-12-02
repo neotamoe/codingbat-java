4d8c587de0e85a4e7892261e050a783bc275a253
Neota Moe <neota.moe@gmail.com>
2017-12-02 14:28:43 -0600

    three more

---
+++ Recursion-1/changePi.java
@@ -0,0 +1,10 @@
+// Given a string, compute recursively (no loops) a new string where all appearances
+// of "pi" have been replaced by "3.14".
+
+
+public String changePi(String str) {
+  if(str.length()==0){ return ""; }
+  if(str.length()<2){ return str.substring(0); }
+  if(str.substring(0,2).equals("pi")) { return "3.14" + changePi(str.substring(2)); }
+  return str.charAt(0) + changePi(str.substring(1));
+}

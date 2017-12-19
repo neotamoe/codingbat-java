dc125f5a11482c7bd3118bc3c31a24d5fca238b4
Neota Moe <neota.moe@gmail.com>
2017-12-18 18:20:10 -0600

    one

---
+++ Array-2/has77.java
@@ -0,0 +1,20 @@
+// Given an array of ints, return true if the array contains two 7's next to each
+// other, or there are two 7's separated by one element, such as with {7, 1, 7}.
+//
+// has77([1, 7, 7]) → true
+// has77([1, 7, 1, 7]) → true
+// has77([1, 7, 1, 1, 7]) → false
+
+public boolean has77(int[] nums) {
+  boolean result = false;
+  for(int i=0; i<nums.length-1; i++){
+    if(nums[i]==7){
+      if(nums[i+1]==7){
+        result = true;
+      } else if (i<=nums.length-3 && nums[i+2]==7){
+        result = true;
+      }
+    }
+  }
+  return result;
+}

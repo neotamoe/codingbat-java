daa15a6b2b5e82d05e3fd21e35e99e12b61cdf3c
Neota Moe <neota.moe@gmail.com>
2017-12-18 19:59:13 -0600

    more

---
+++ Array-2/shiftLeft.java
@@ -0,0 +1,18 @@
+// Return an array that is "left shifted" by one -- so {6, 2, 5, 3} returns
+// {2, 5, 3, 6}. You may modify and return the given array, or return a new array.
+//
+// shiftLeft([6, 2, 5, 3]) → [2, 5, 3, 6]
+// shiftLeft([1, 2]) → [2, 1]
+// shiftLeft([1]) → [1]
+
+public int[] shiftLeft(int[] nums) {
+  int[] shifted = new int[nums.length];
+  for(int i=0; i<nums.length; i++){
+    if(i!=nums.length-1){
+      shifted[i]=nums[i+1];
+    } else {
+      shifted[i]=nums[0];
+    }
+  }
+  return shifted;
+}
---
+++ Array-2/tenRun.java
@@ -0,0 +1,27 @@
+// For each multiple of 10 in the given array, change all the values following it
+// to be that multiple of 10, until encountering another multiple of 10. So
+// {2, 10, 3, 4, 20, 5} yields {2, 10, 10, 10, 20, 20}.
+//
+// tenRun([2, 10, 3, 4, 20, 5]) → [2, 10, 10, 10, 20, 20]
+// tenRun([10, 1, 20, 2]) → [10, 10, 20, 20]
+// tenRun([10, 1, 9, 20]) → [10, 10, 10, 20]
+public int[] tenRun(int[] nums) {
+  int[] newTen = new int[nums.length];
+  int temp = 0;
+  boolean found = false;
+  for(int i=0; i<nums.length; i++){
+    if(nums[i]%10!=0 && !found){
+      newTen[i]=nums[i];
+    } else if (nums[i]%10==0 && !found){
+      temp = nums[i];
+      found = !found;
+      newTen[i]=nums[i];
+    } else if (nums[i]%10!=0 && found){
+      newTen[i] = temp;
+    } else if (nums[i]%10==0 && found){
+      temp = nums[i];
+      newTen[i]=nums[i];
+    }
+  }
+  return newTen;
+}

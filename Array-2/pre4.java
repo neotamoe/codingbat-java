ea5e146cfbb196b3a72c88ec1759ab50ce2f0031
Neota Moe <neota.moe@gmail.com>
2017-12-20 19:03:58 -0600

    one

---
+++ Array-2/pre4.java
@@ -0,0 +1,22 @@
+// Given a non-empty array of ints, return a new array containing the elements from
+// the original array that come before the first 4 in the original array. The
+// original array will contain at least one 4. Note that it is valid in java to
+// create an array of length 0.
+//
+// pre4([1, 2, 4, 1]) → [1, 2]
+// pre4([3, 1, 4]) → [3, 1]
+// pre4([1, 4, 4]) → [1]
+public int[] pre4(int[] nums) {
+  int fourIndex = 0;
+  for(int i=0; i<nums.length; i++){
+    if(nums[i]==4){
+      fourIndex = i;
+      break;
+    }
+  }
+  int[] firstFour = new int[fourIndex];
+  for(int i=0; i<firstFour.length; i++){
+    firstFour[i] = nums[i];
+  }
+  return firstFour;
+}

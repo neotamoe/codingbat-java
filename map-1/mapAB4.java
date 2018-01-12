e4c308960501fbd801ab31e5bc8453e9eaf01b9e
Neota Moe <neota.moe@gmail.com>
2018-01-12 17:58:47 -0600

    friday woo hoo

---
+++ map-1/mapAB4.java
@@ -0,0 +1,23 @@
+// Modify and return the given map as follows: if the keys "a" and "b" have values
+// that have different lengths, then set "c" to have the longer value. If the values
+// exist and have the same length, change them both to the empty string in the map.
+//
+// mapAB4({"a": "aaa", "b": "bb", "c": "cake"}) → {"a": "aaa", "b": "bb", "c": "aaa"}
+// mapAB4({"a": "aa", "b": "bbb", "c": "cake"}) → {"a": "aa", "b": "bbb", "c": "bbb"}
+// mapAB4({"a": "aa", "b": "bbb"}) → {"a": "aa", "b": "bbb", "c": "bbb"}
+
+public Map<String, String> mapAB4(Map<String, String> map) {
+  if(map.get("a")!=null && map.get("b")!=null){
+    if (map.get("a").length() > map.get("b").length() ) {
+      map.put("c", map.get("a"));
+    } else if (map.get("b").length() > map.get("a").length() ) {
+      map.put("c", map.get("b"));
+    } else {
+      map.put("a","");
+      map.put("b", "");
+    }
+  }
+
+
+  return map;
+}

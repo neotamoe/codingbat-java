43f4a0c6954d15e2376a8bf5d27468cad8130f95
Neota Moe <neota.moe@gmail.com>
2018-01-20 08:09:19 -0600

    filter

---
+++ Functional-2/noZ.java
@@ -0,0 +1,11 @@
+// Given a list of strings, return a list of the strings, omitting any string that contains a "z". (Note: the str.contains(x) method returns a boolean)
+//
+// noZ(["aaa", "bbb", "aza"]) → ["aaa", "bbb"]
+// noZ(["hziz", "hzello", "hi"]) → ["hi"]
+// noZ(["hello", "howz", "are", "youz"]) → ["hello", "are"]
+
+public List<String> noZ(List<String> strings) {
+  return strings.stream()
+                .filter(n -> !n.contains("z"))
+                .collect(Collectors.toList());
+}
